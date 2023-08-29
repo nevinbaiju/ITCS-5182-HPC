d5890d6baef70fa169b53791aa700d63f8ce9273
Nevin Baiju <nevinbaiju@gmail.com>
2023-08-29 20:55:35 +0530

    Advection initial commit

---
+++ advection/advection.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+int main(int argc, char* argv[])
+{
+    // printf("You have entered %d arguments:\n", argc);
+ 
+    // for (int i = 0; i < argc; i++) {
+    //     printf("%s\n", argv[i]);
+    // }
+    // return 0;
+
+    int n = atoi(argv[1]);
+    float dt = atof(argv[2]);
+
+    float t_max = 2.0;
+    float x_min = 0.0;
+    float x_max = 1.0;
+    float v = 1;
+    float x_c = 0.25;
+
+    float dx = (x_max - x_min) / 2;
+    float nb_steps = t_max/dt;
+    float alpha = (v*dt)/(2*dx);
+
+    float x[n+2];
+    float u_0[n+2];
+    float u[n+2];
+    float u_new[n+2];
+    //xmin + (i − 1) ∗ dx, ∀i ∈ {0 . . . N + 2};
+    for(int i=0; i<=n+2; i++){
+        x[i] = x_min + (i-1) * dx;
+    }
+    //u0[i] = e−200∗(x[i]−xc)2, ∀i ∈ {0 . . . N + 2};
+    for(int i=0; i<=n+2; i++){
+        u_0[i] = std::exp(-200*(x[i] - x_c)*(x[i] - x_c));
+    }
+    memcpy(u, u_0, (n+2)*sizeof(float));
+    memcpy(u_new, u_0, (n+2)*sizeof(float)); 
+
+    int current_time;
+
+    for (int timestamp=0; timestamp<nb_steps; timestamp++){
+        current_time = timestamp*dt;
+        for (int j=1; j<n+2; j++){
+            u_new[j] = u[j] - alpha*(u[j+1] - u[j-1]) + 0.5*(u[j+1] - 2*u[j] + u[j-1]);
+        }
+        memcpy(u, u_new, (n+2)*sizeof(float));
+
+        u[0] = u[n+1];
+        u[n+2] = u[1];
+        // std::cout << u[0] << " " << u[n+2] << std::endl;
+        if (timestamp%1000 == 0){
+            std::cout << timestamp << " " << nb_steps << "\n";
+        }
+    }
+
+    std::cout << nb_steps << std::endl;
+    std::cout << u[0] << " " << u[n+2] << std::endl;
+}
+
+
---
+++ advection/run.sh
@@ -0,0 +1,3 @@
+rm advection.o
+g++ advection.cpp -o advection.o
+./advection.o 10000 0.000003
