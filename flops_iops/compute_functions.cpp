149e054b997453050ed644c2d9e7724483ef579e
Nevin <nevinbaiju@gmail.com>
2023-09-21 12:43:31 -0400

    loop unrolling

--- flops_iops/compute_functions.cpp
+++ flops_iops/compute_functions.cpp
@@ -38,22 +38,24 @@
 // }
 
 void perform_addition(__m256 a, __m256 b, long int n, float result[], float temp_result[]) {
-    __m256 _mm_a = _mm256_set1_ps(0.0);
-    __m256 _mm_b = _mm256_set1_ps(1.0);
     #pragma omp parallel
     {
-        for (int i = 0; i < n; i ++) {
-            _mm256_add_ps(_mm_a, _mm_b);
-            // __m256 temp_result = _mm256_add_ps(_mm256_loadu_ps(&a[i]), _mm256_loadu_ps(&b[i]));
-            // _mm256_storeu_ps(&result[i], temp_result);
+        __m256 _mm_a = _mm256_setzero_ps();
+        __m256 _mm_b = _mm256_set_ps(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0);
+
+        __m256 _mm_c = _mm256_setzero_ps();
+        __m256 _mm_d = _mm256_set_ps(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0);
+        for (int i = 0; i < n; i +=2) {
+            _mm_a = _mm256_add_ps(_mm_a, _mm_b);
+            _mm_c = _mm256_mul_ps(_mm_c, _mm_d);
         }
         #pragma omp critical
         {
             _mm256_store_ps(&temp_result[0], _mm_a);
-            result[0] += temp_result[0];
-            // for(int i=0; i<8; i++){
-            //     result[i] += temp_result[i];
-            // }
+            // result[0] += temp_result[0];
+            for(int i=0; i<8; i++){
+                result[i] += temp_result[i];
+            }
         }
     }
     std::cout << "Done: " << result[0] << std::endl;
