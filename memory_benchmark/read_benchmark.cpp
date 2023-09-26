fd467960fcb3650fb587ddbc380a9cfadf709f60
Nevin Baiju <nevinbaiju@gmail.com>
2023-09-26 13:27:55 -0400

    Code optimizations

--- memory_benchmark/read_benchmark.cpp
+++ memory_benchmark/read_benchmark.cpp
@@ -3,10 +3,18 @@
 #include <stdlib.h>
 #include <chrono>
 #include <immintrin.h>  // AVX intrinsics
+#include <string>
 
-#define ARRAY_SIZE (200*(1024*1024))/4  // Adjust the array size as needed
+// #define ARRAY_SIZE (2000*(1024*1024))/4  // Adjust the array size as needed
 
-int main() {
+int main(int argc, char *argv[]) {
+    
+    if (argc < 2){
+        std::cout << "Usage: ./read_benchmark <memory size (mb)>\n";
+        exit(0);
+    }
+    int mem_size = std::stoi(argv[1]);
+
     int *array;
     int allocate_status = posix_memalign((void **)&array, 32, 8 * 32);
     for(int i=0; i<8; i++){
@@ -15,8 +23,10 @@ int main() {
     unsigned int sum = 0;
     __m256i _m_sum = _mm256_setzero_si256();
     int sum_array[8];
+
+    ////////////////////// Timing block /////////////////////////////////////////////////////
     auto start = std::chrono::high_resolution_clock::now();
-    for (int i = 0; i < ARRAY_SIZE; i += 8) {
+    for (int i = 0; i < mem_size; i += 8) {
         __m256i data = _mm256_stream_load_si256((__m256i *)&array[0]);
         _m_sum = _mm256_add_epi32(_m_sum, data);
         _mm256_storeu_si256((__m256i *)sum_array, _m_sum);
@@ -26,11 +36,13 @@ int main() {
         }
     }
     auto end = std::chrono::high_resolution_clock::now();
+    /////////////////////////////////////////////////////////////////////////////////////////
+    
     std::chrono::duration<double> elapsed_seconds = end - start;
     double seconds = elapsed_seconds.count();   
     
     std::cout << "Sum: " << sum << std::endl;
-    double read_bandwidth = (ARRAY_SIZE * sizeof(int)) / (seconds * 1024 * 1024 * 1024); // MB/s
+    double read_bandwidth = (mem_size * sizeof(int)) / (seconds * 1024 * 1024 * 1024); // MB/s
 
     std::cout << "Read Bandwidth: " << read_bandwidth << " GB/s\n";
 
--- memory_benchmark/write_benchmark.cpp
+++ memory_benchmark/write_benchmark.cpp
@@ -4,28 +4,38 @@
 #include <chrono>
 #include <immintrin.h>  // AVX intrinsics
 
-#define ARRAY_SIZE (200*(1024*1024))/4  // Adjust the array size as needed
-
-int main() {
-    int *array;
-    int n=32000000;
-    int allocate_status = posix_memalign((void **)&array, 32, n * 32);
-    __m256i zero_vals = _mm256_setzero_si256();
-    auto start = std::chrono::high_resolution_clock::now();
-    // for (int i = 0; i < ARRAY_SIZE; i += 8) {
-    //     _mm256_storeu_si256((__m256i *)&array[i], zero_vals);
-    // }
-    for (int i = 0; i < ARRAY_SIZE; i ++) {
-        array[i] = 0;
+int main(int argc, char *argv[]) {
+    
+    if (argc < 2){
+        std::cout << "Usage: ./read_benchmark <memory size (kb)>\n";
+        exit(0);
     }
+    int arr_size = int((std::stoi(argv[1])*1024)/4);
+    std::cout << "Array size: " << (std::stoi(argv[1])*1024)/4 << std::endl;
+
+    int *array;
+    int allocate_status = posix_memalign((void **)&array, 32, arr_size * 32);
+    __m256i zero_vals = _mm256_setzero_si256();
+
+    ////////////////////// Timing block /////////////////////////////////////////////////////
+    auto start = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < arr_size; i += 8) {
+        _mm256_storeu_si256((__m256i *)&array[i], zero_vals);
+    }
+    // for (int i = 0; i < arr_size; i ++) {
+    //     array[i] = 0;
+    // }
     auto end = std::chrono::high_resolution_clock::now();
+    /////////////////////////////////////////////////////////////////////////////////////////
+    
     std::chrono::duration<double> elapsed_seconds = end - start;
     double seconds = elapsed_seconds.count();   
     
     std::cout << "val: " << array[0] << std::endl;
-    double read_bandwidth = (ARRAY_SIZE * sizeof(int)) / (seconds * 1024 * 1024 * 1024); // MB/s
+    double read_bandwidth = (arr_size * sizeof(int)) / (seconds * 1024 * 1024 * 1024); // MB/s
 
     std::cout << "Write Bandwidth: " << read_bandwidth << " GB/s\n";
+    free(array);
 
     return 0;
 }
