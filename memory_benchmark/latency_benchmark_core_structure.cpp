c08393e568d19bd62fcb233c52a19f84896a3410
Nevin Baiju <nevinbaiju@gmail.com>
2023-09-27 23:45:30 -0400

    latest code

--- memory_benchmark/latency_benchmark_core_structure.cpp
+++ memory_benchmark/latency_benchmark_core_structure.cpp
@@ -7,22 +7,38 @@ struct Node {
     struct Node * next;
 };
 
+const int CACHE_LINE = 2048;
+
 int main(int argc, char* argv[]){
     if (argc < 2){
         std::cout << "Usage: ./write_benchmark <memory size (kb)>\n";
         exit(0);
     }
-    Node *node_arr;
     int n = std::stoi(argv[1])*(1024/sizeof(struct Node));
-    int allocate_status = posix_memalign((void **)&node_arr, sizeof(Node), n * sizeof(Node));
+    // int allocate_status = posix_memalign((void **)&node_arr, sizeof(Node), n * sizeof(Node));
 
-    for(int i=0; i<n; i++){
+    int nodes_per_cache = CACHE_LINE/sizeof(struct Node);
+    int total_chunks = n/nodes_per_cache;
+
+    
+    Node *node_arr;
+    int allocate_status = posix_memalign((void **)&node_arr, nodes_per_cache * sizeof(Node), nodes_per_cache * sizeof(Node));
+
+    for(int i=0; i<nodes_per_cache; i++){
         node_arr[i].next = &node_arr[i];
     }
+    node_arr[nodes_per_cache-1].next = &node_arr[0];
 
     auto start = std::chrono::high_resolution_clock::now();
-    for(int i=0; i<n; i++){
-        node_arr[i].next;
+    for(int i=0; i<n; i+=nodes_per_cache){
+        node_arr[0].next;
+        node_arr[1].next;
+        node_arr[2].next;
+        node_arr[3].next;
+        node_arr[4].next;
+        node_arr[5].next;
+        node_arr[6].next;
+        node_arr[7].next;
     }
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double, std::nano> elapsed_seconds = end - start;
--- memory_benchmark/read_benchmark.cpp
+++ memory_benchmark/read_benchmark.cpp
@@ -11,13 +11,13 @@
 int main(int argc, char *argv[]) {
     
     if (argc < 2){
-        std::cout << "Usage: ./read_benchmark <memory size (mb)>\n";
+        std::cout << "Usage: ./read_benchmark <memory size (kb)>\n";
         exit(0);
     }
-    int mem_size = std::stoi(argv[1]);
+    int arr_size = (std::stoi(argv[1])*1024)/sizeof(int);
 
     int *array;
-    int allocate_status = posix_memalign((void **)&array, 32, 8 * 32);
+    int allocate_status = posix_memalign((void **)&array, 32, arr_size * 32);
     for(int i=0; i<8; i++){
         array[i] = i;
     }
@@ -26,26 +26,28 @@ int main(int argc, char *argv[]) {
     __m256i data;
     int sum_array[8];
 
+    for (int i = 0; i < arr_size; i += 8) {
+        data = _mm256_stream_load_si256((__m256i *)&array[0]);
+        _m_sum = _mm256_add_epi32(_m_sum, data);
+    }
     ////////////////////// Timing block /////////////////////////////////////////////////////
     auto start = std::chrono::high_resolution_clock::now();
     #pragma omp for
-    for (int i = 0; i < mem_size; i += 8) {
+    for (int i = 0; i < arr_size; i += 8) {
         data = _mm256_stream_load_si256((__m256i *)&array[0]);
         _m_sum = _mm256_add_epi32(_m_sum, data);
-        _mm256_storeu_si256((__m256i *)sum_array, _m_sum);
-    
-        for (int j = 0; j < 8; j++) {
-            sum += sum_array[j];
-        }
     }
     auto end = std::chrono::high_resolution_clock::now();
     /////////////////////////////////////////////////////////////////////////////////////////
-
+    _mm256_storeu_si256((__m256i *)sum_array, _m_sum);
+    for (int j = 0; j < 8; j++) {
+            sum += sum_array[j];
+    }
     std::chrono::duration<double> elapsed_seconds = end - start;
     double seconds = elapsed_seconds.count();   
     
     std::cout << "Sum: " << sum << std::endl;
-    double read_bandwidth = (mem_size * sizeof(int)) / (seconds * 1024 * 1024 * 1024); // MB/s
+    double read_bandwidth = (arr_size * sizeof(int)) / (seconds * 1024 * 1024 * 1024); 
 
     std::cout << "Read Bandwidth: " << read_bandwidth << " GB/s\n";
 
