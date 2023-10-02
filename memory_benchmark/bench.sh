147dfc3fe2ca2394436ad09f459505ee09a99f57
Nevin <nevinbaiju@gmail.com>
2023-10-02 18:34:58 -0400

    modification of build scripts

--- memory_benchmark/bench.sh
+++ memory_benchmark/bench.sh
@@ -3,8 +3,16 @@
 iterate_and_run() {
   local program=$1
 
+  rm results/results_"$program".txt
+
   # Iterate from 1KB to 200MB in increments of 512KB
   ./"$program" 1 2>> results/results_"$program".txt
+  
+  for ((size_kb = 16; size_kb <= 513; size_kb += 16)); do
+    echo "Running for: $size_kb KB"
+    ./"$program" "$size_kb" 2>> results/results_"$program".txt
+  done
+
   for ((size_kb = 512; size_kb <= 204801; size_kb += 512)); do
     echo "Running for: $size_kb KB"
     ./"$program" "$size_kb" 2>> results/results_"$program".txt
@@ -16,8 +24,8 @@ mkdir plots
 
 # Call the function with the provided program name
 iterate_and_run "read"
-iterate_and_run "write"
-iterate_and_run "read_write"
-iterate_and_run "latency"
+# iterate_and_run "write"
+# iterate_and_run "read_write"
+# iterate_and_run "latency"
 
 python plot_results.py
--- memory_benchmark/plot_results.py
+++ memory_benchmark/plot_results.py
@@ -5,7 +5,7 @@ def plot(experiment):
     with open(f'results/results_{experiment}.txt', 'r') as file:
         results = file.read()
     y = np.array(results.split('\n')[:-1]).astype('float')
-    x = [1] + [x for x in range(512, 204801, 512)]
+    x = [1] + [x for x in range(16, 513, 16)] + [x for x in range(512, 204801, 512)]
     
     if 'latency' in experiment:
         formatted_exp_name = "-".join(experiment.split('_')).title()
@@ -22,11 +22,25 @@ def plot(experiment):
     plt.title(title)
     plt.xlabel(x_label)
     plt.ylabel(y_label)
+    plt.axvline(3072, label='L3', color='red')
+    plt.legend()
     
     plt.savefig(f'plots/{experiment}.png')
     plt.clf()
 
+    plt.plot(x[:40], y[:40])
+    plt.axvline(64, label='L1', color='green')
+    plt.axvline(512, label='L2', color='blue')
+    plt.axvline(3072, label='L3', color='orange')
+    plt.title(title + " Cache effects")
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.legend()
+    
+    plt.savefig(f'plots/{experiment}_cache.png')
+    plt.clf()
+
 plot('read')
-plot('write')
-plot('read_write')
-plot('latency')
+# plot('write')
+# plot('read_write')
+# plot('latency')
