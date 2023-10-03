15fd4b7a7217a81a7c7a71b3f391110cc55e74b3
Nevin Baiju <nevinbaiju@gmail.com>
2023-10-02 23:06:00 -0400

    fixed get num threads bug

--- memory_benchmark/write_benchmark.cpp
+++ memory_benchmark/write_benchmark.cpp
@@ -14,16 +14,21 @@ int main(int argc, char *argv[]) {
         std::cout << "Usage: ./read_benchmark <memory size (kb)>\n";
         exit(0);
     }
-    int num_threads = omp_get_num_threads();
+    int num_threads;
+    #pragma omp parallel
+    {
+        #pragma omp single
+        num_threads = omp_get_num_threads();
+    }
     int arr_size = (std::stoi(argv[1])*1024)/(sizeof(int));
 
     int *array;
-    int allocate_status = posix_memalign((void **)&array, 32*8*4, arr_size * 32);
-    int nbiter = 25;
+    int allocate_status = posix_memalign((void **)&array, 32*8*4, arr_size * 32 + 1);
+    int nbiter = 50;
     double seconds = 0;
 
     ////////////////////// Timing block /////////////////////////////////////////////////////
-
+    
     #pragma omp parallel
     {
         int i;
@@ -35,9 +40,15 @@ int main(int argc, char *argv[]) {
         __m256i data_3 = _mm256_setzero_si256();
         __m256i data_4 = _mm256_setzero_si256();
         auto start = std::chrono::high_resolution_clock::now();
-        std::cout << "TID: " << tid << chuck_start << ":" << chuck_end << std::endl;
+        // #pragma omp critical
+        // {
+        //     std::cout << "Num threads: " << num_threads << std::endl;
+        //     std::cout << "TID: " << tid << ", "<< chuck_start << ":" << chuck_end << std::endl;
+        // }
         for(int iter=0; iter<nbiter; iter++){ 
+            // std::cout << iter << " " << tid << std::endl;
             for (i=chuck_start; i<chuck_end; i+=32){
+                // std::cout << iter << " " << tid << " " << i << "/" << chuck_end << std::endl;
                 _mm256_storeu_si256((__m256i *)&array[i], data_1);
                 _mm256_storeu_si256((__m256i *)&array[i+8], data_2);
                 _mm256_storeu_si256((__m256i *)&array[i+16], data_3);
