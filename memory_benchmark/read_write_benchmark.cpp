a651cf532a475cac6af3721f8af370bc83b7ed34
Nevin <nevinbaiju@gmail.com>
2023-10-03 21:53:59 -0400

    read_write_benchmark.cpp

--- memory_benchmark/read_write_benchmark.cpp
+++ memory_benchmark/read_write_benchmark.cpp
@@ -3,6 +3,10 @@
 #include <stdlib.h>
 #include <chrono>
 #include <immintrin.h>  // AVX intrinsics
+#include <string>
+#include <omp.h>
+
+// #define ARRAY_SIZE (2000*(1024*1024))/4  // Adjust the array size as needed
 
 int main(int argc, char *argv[]) {
     
@@ -10,44 +14,72 @@ int main(int argc, char *argv[]) {
         std::cout << "Usage: ./read_write_benchmark <memory size (kb)>\n";
         exit(0);
     }
-    int arr_size = int((std::stoi(argv[1])*1024)/4);
-    std::cout << "Array size: " << (std::stoi(argv[1])*1024)/4 << std::endl;
+    int num_threads;
+    #pragma omp parallel
+    {
+        #pragma omp single
+        num_threads = omp_get_num_threads();
+    }
+    int arr_size = (std::stoi(argv[1])*1024)/(sizeof(int));
 
-    int *arr1, *arr2;
-    int allocate_status1 = posix_memalign((void **)&arr1, 32, arr_size * 32);
-    int allocate_status2 = posix_memalign((void **)&arr2, 32, arr_size * 32);
-    
-    for (int i=0; i < arr_size; i++){
-        arr1[i] = 1;
+    int *array1, *array2;
+    int allocate_status;
+    allocate_status = posix_memalign((void **)&array1, 32*8*4, arr_size * 32);
+    allocate_status = posix_memalign((void **)&array2, 32*8*4, arr_size * 32);
+    int nbiter = 50;
+    double seconds = 0;
+
+    for(int i=0; i<arr_size; i++){
+        array1[i] = 1;
     }
 
-    __m256i copy_data;
     ////////////////////// Timing block /////////////////////////////////////////////////////
-    auto start = std::chrono::high_resolution_clock::now();
-    for (int i = 0; i < arr_size; i += 8/**4*/) {
-        copy_data = _mm256_stream_load_si256((__m256i *)&arr1[i]);
-        _mm256_storeu_si256((__m256i *)&arr2[i], copy_data);
-        // _mm256_storeu_si256((__m256i *)&array[i+8], zero_vals);
-        // _mm256_storeu_si256((__m256i *)&array[i+16], zero_vals);
-        // _mm256_storeu_si256((__m256i *)&array[i+24], zero_vals);
+    
+    #pragma omp parallel
+    {
+        int i;
+        int tid = omp_get_thread_num();
+        int chuck_start = tid*(arr_size/num_threads);
+        int chuck_end = (tid+1)*(arr_size/num_threads);
+        __m256i reg1;
+        __m256i reg2;
+        __m256i reg3;
+        __m256i reg4;
+        auto start = std::chrono::high_resolution_clock::now();
+        // #pragma omp critical
+        // {
+        //     std::cout << "Num threads: " << num_threads << std::endl;
+        //     std::cout << "TID: " << tid << ", "<< chuck_start << ":" << chuck_end << std::endl;
+        // }
+        for(int iter=0; iter<nbiter; iter++){ 
+            for (i=chuck_start; i<chuck_end; i+=32){
+                reg1 = _mm256_stream_load_si256((__m256i *)&array1[i]);
+                reg2 = _mm256_stream_load_si256((__m256i *)&array1[i+8]);
+                reg3 = _mm256_stream_load_si256((__m256i *)&array1[i+16]);
+                reg4 = _mm256_stream_load_si256((__m256i *)&array1[i+24]);
+
+                _mm256_storeu_si256((__m256i *)&array2[i], reg1);
+                _mm256_storeu_si256((__m256i *)&array2[i+8], reg2);
+                _mm256_storeu_si256((__m256i *)&array2[i+16], reg3);
+                _mm256_storeu_si256((__m256i *)&array2[i+24], reg4);
+            }
+        }
+        auto end = std::chrono::high_resolution_clock::now();
+        #pragma omp critical
+        {
+            std::chrono::duration<double> elapsed_seconds = end - start;
+            seconds += elapsed_seconds.count();
+        }
     }
-    // for (int i = 0; i < arr_size; i ++) {
-    //     array[i] = 0;
-    // }
-    auto end = std::chrono::high_resolution_clock::now();
-    /////////////////////////////////////////////////////////////////////////////////////////
-
-    std::chrono::duration<double> elapsed_seconds = end - start;
-    double seconds = elapsed_seconds.count();   
     
-    std::cout << "val: " << arr2[0] << std::endl;
-    double read_bandwidth = (arr_size * sizeof(int)) / (seconds * 1024 * 1024 * 1024); // MB/s
+    std::cout << "val: " << array2[0] << std::endl;
+    double read_bandwidth = (nbiter*arr_size* num_threads * sizeof(int)) / (seconds * 1024 * 1024 * 1024); 
 
-    std::cout << "Read-Write Bandwidth: " << read_bandwidth << " GB/s\n";
+    std::cout << "Write Bandwidth: " << read_bandwidth << " GB/s\n";
     std::cerr << read_bandwidth << std::endl;
-    
-    free(arr1);
-    free(arr2);
+
+    free(array1);
+    free(array2);
 
     return 0;
 }
