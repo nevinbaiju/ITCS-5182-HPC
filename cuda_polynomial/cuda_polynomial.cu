5ee0556f81aa5c69eddfb229b281ee735e90df78
Nevin Baiju <nevinbaiju@gmail.com>
2023-10-20 20:19:28 -0400

    Coded the right answer

--- cuda_polynomial/cuda_polynomial.cu
+++ cuda_polynomial/cuda_polynomial.cu
@@ -1,54 +1,96 @@
 #include <stdio.h>
+#include<iostream>
+#include <cmath> 
+#include <cuda_runtime.h>
 
-// CUDA kernel to add 1 to every element of the array
-__global__ void addOneToFloatArray(float* array, int n) {
-    int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    if (idx < n) {
-        array[idx] = idx;
+#define checkCudaErrors(cudaCall)                                                    \
+{                                                                                  \
+    cudaError_t error = cudaCall;                                                  \
+    if (error != cudaSuccess)                                                      \
+    {                                                                              \
+        fprintf(stderr, "CUDA error in file '%s' in line %i: %s\n",                \
+                __FILE__, __LINE__, cudaGetErrorString(error));                     \
+        exit(1);                                                                   \
+    }                                                                              \
+}
+
+void validate_res(float *res, int n, int degree){
+    for(int i=0; i<n; i++){
+        if(std::abs(res[i]-(degree+1)) > 1){
+            std::cerr << "Error: The calculation is wrong!\n";
+            break;
+        }
     }
 }
 
+void compute_poly(float* array, int n, float* poly, int degree, float* result){
+    for(int i=0; i<n; i++){
+        float x=array[i];
+        float x_tothepowerof=1;
+        float out=0;
+        for(int j=0; j<degree; j++){
+            out += poly[j]*x_tothepowerof;
+            x_tothepowerof *=x;
+        }
+        result[i] = out;
+    }
+}
+
+__global__ void compute_poly_gpu(float *array, int n, float *poly, int degree) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx < n){
+        float out = 0;
+        float x = array[idx];
+        float x_tothepowerof=1;
+        for(int i=0; i<degree; i++){
+            out += x_tothepowerof*poly[i];
+            x_tothepowerof *= x;
+        }
+        array[idx] = out;
+    }
+}
+
+
 int main() {
-    int n = 10;  // Size of the float array
-    float* h_array = new float[n];  // Host (CPU) array
-
-    // Initialize the host array
+    int n = 10000;
+    float* h_array = new float[n];
+    float* h_res1 = new float[n];
+    float* h_res2 = new float[n];
+    
+    int degree = 100;
+    float* h_poly = new float[degree];
+    
     for (int i = 0; i < n; i++) {
-        h_array[i] = 0;
+        h_array[i] = 1;
+    }
+    for (int i = 0; i < degree; i++) {
+        h_poly[i] = 1;
     }
 
-    for (int i = 0; i < n; i++) {
-        printf("Initialized h_array[%d] = %.2f\n", i, h_array[i]);
-    }
-
-    float* d_array;  // Device (GPU) array
-
-    // Allocate memory on the GPU for the array
-    cudaMalloc((void**)&d_array, n * sizeof(float));
-    // Initialize the GPU array with zeros
-    cudaMemset(d_array, 0, n * sizeof(float));
-
-    // Copy data from the host to the device
+    float *d_array, *d_poly;
+    cudaMalloc(&d_array, n*sizeof(float));
+    cudaMalloc(&d_poly, degree*sizeof(float));
     cudaMemcpy(d_array, h_array, n * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_poly, h_poly, degree * sizeof(float), cudaMemcpyHostToDevice);
 
-    // Define the grid and block dimensions
-    int threadsPerBlock = 8;
+    int threadsPerBlock = 256;
     int blocksPerGrid = (n + threadsPerBlock - 1) / threadsPerBlock;
+    compute_poly_gpu<<<blocksPerGrid, threadsPerBlock>>>(d_array, n, d_poly, degree);
+    cudaMemcpy(h_res1, d_array, n * sizeof(float), cudaMemcpyDeviceToHost);
 
-    // Launch the CUDA kernel to add 1 to each element
-    addOneToFloatArray<<<blocksPerGrid, threadsPerBlock>>>(d_array, n);
-
-    // Copy the result back from the device to the host
-    cudaMemcpy(h_array, d_array, n * sizeof(float), cudaMemcpyDeviceToHost);
-
-    // Clean up
+    compute_poly(h_array, n, h_poly, degree, h_res2);
+    
+    
+    validate_res(h_res1, n, degree);
+    validate_res(h_res2, n, degree);
+    
     cudaFree(d_array);
+    cudaFree(d_poly);
+
     delete[] h_array;
-
-    // Print the updated array
-    for (int i = 0; i < n; i++) {
-        printf("Updated h_array[%d] = %.2f\n", i, h_array[i]);
-    }
-
+    delete[] h_poly;
+    delete[] h_res1;
+    delete[] h_res2;
+    
     return 0;
 }
