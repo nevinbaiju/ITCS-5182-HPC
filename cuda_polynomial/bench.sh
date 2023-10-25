b23495e4da64dea035d99130c2833bf4267bc0fb
Nevin <nevinbaiju@gmail.com>
2023-10-25 15:05:03 -0400

    centaurus bench codes

--- cuda_polynomial/bench.sh
+++ cuda_polynomial/bench.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-for ((n = $1; n <= $2; n += $3)); do
-    for ((deg = $4; deg <= $5; deg += 10)); do
-        echo "Running for array size: $n, Degree: $deg, experiment: $5"
-        ./cuda_polynomial $n $deg 2>> results/$6.txt
+for ((iter = 0; iter < $7; iter += 1)); do
+    for ((n = $1; n <= $2; n += $3)); do
+        for ((deg = $4; deg <= $5; deg += 10)); do
+            echo "Running for array size: $n, Degree: $deg, experiment: $6"
+            ./cuda_polynomial $n $deg 2>> results/$6.txt
+        done
     done
 done
--- cuda_polynomial/queue.sh
+++ cuda_polynomial/queue.sh
@@ -7,15 +7,11 @@ rm results/*
 rm plots/*
 
 # # PCI-E latency
-for ((i = 0; n <= 50; n += 1)); do
-    ./bench.sh 1 1 1 1 1 pci_l
-done
+./bench.sh 1 1 1 1 1 pci_l 50
 
 # # Bandwidth
-./bench.sh 5000  5120000 5000 1 1 bw_1
-./bench.sh 5120000 52400000 100000 1 1 bw_2
+./bench.sh 5000  5120000 5000 1 1 bw_1 1
+./bench.sh 5120000 52400000 100000 1 1 bw_2 1
 
 # FLOPS
-for ((i = 0; n <= 50; n += 1)); do
-    ./bench.sh 100000 100000 100000 10000 10000 flops
-done
+./bench.sh 100000 100000 100000 10000 10000 flops 50
