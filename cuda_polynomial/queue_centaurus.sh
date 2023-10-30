add088b8d2f7ca7449635041541efd1aaac1a1e0
Nevin <nevinbaiju@gmail.com>
2023-10-30 15:24:04 -0400

    queue update

--- cuda_polynomial/queue_centaurus.sh
+++ cuda_polynomial/queue_centaurus.sh
@@ -7,11 +7,11 @@ rm results/*
 rm plots/*
 
 # # PCI-E latency
-# sbatch --partition=GPU --chdir=`pwd` --time=04:00:00 --ntasks=1 --cpus-per-task=16 --gpus-per-task=1 --job-name=pci_l --mem=10G ./bench.sh 1 1 1 1 1 pci_l 10000
+sbatch --partition=GPU --chdir=`pwd` --time=04:00:00 --ntasks=1 --cpus-per-task=16 --gpus-per-task=1 --job-name=pci_l --mem=10G ./bench.sh 1 1 1 1 1 pci_l 10000
 
 # # Bandwidth
-# sbatch --partition=GPU --chdir=`pwd` --time=04:00:00 --ntasks=1 --cpus-per-task=16 --gpus-per-task=1 --job-name=bw_1 --mem=10G ./bench.sh 5000  5120000 5000 1 1 bw_1 1
+sbatch --partition=GPU --chdir=`pwd` --time=04:00:00 --ntasks=1 --cpus-per-task=16 --gpus-per-task=1 --job-name=bw_1 --mem=10G ./bench.sh 5000  5120000 5000 1 1 bw_1 1
 sbatch --partition=GPU --chdir=`pwd` --time=04:00:00 --ntasks=1 --cpus-per-task=16 --gpus-per-task=1 --job-name=bw_2 --mem=32G ./bench.sh 5120000 2000000000 10000000 5 5 bw_2 1
 
 # FLOPS
-# sbatch --partition=GPU --chdir=`pwd` --time=04:00:00 --ntasks=1 --cpus-per-task=16 --gpus-per-task=1 --job-name=flops --mem=10G ./bench.sh 100000 100000 100000 10000 10000 flops 50
+sbatch --partition=GPU --chdir=`pwd` --time=04:00:00 --ntasks=1 --cpus-per-task=16 --gpus-per-task=1 --job-name=flops --mem=10G ./bench.sh 100000 100000 100000 10000 10000 flops 50
