7dc8723f62832e2461d07662e84986aa510f2b16
Nevin Baiju <nevinbaiju@gmail.com>
2023-11-16 22:37:28 -0500

    Benching code

--- cuda_pipelined_polynomial/pipelined_polynomial.cu
+++ cuda_pipelined_polynomial/pipelined_polynomial.cu
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <cuda_runtime.h>
+#include <chrono>
+
 
 #define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
 inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
@@ -26,15 +28,25 @@ __global__ void compute_poly(float *d_arr_chunk, float *d_coeffs, float *d_resul
     }
 }
 
+void get_time_elapsed(std::chrono::time_point<std::chrono::high_resolution_clock> start, 
+                        std::chrono::time_point<std::chrono::high_resolution_clock> end, int64_t n, int degree){
+    std::chrono::duration<double> elapsed_seconds = end - start;
+    double seconds = elapsed_seconds.count();
+    int64_t gflop = (((n))*3*(degree + 1))/1e9;
+    double flops =  gflop/(seconds);
+    std::cout << "FLOPS: " << flops << " Giga FLOPS" << std::endl;
+}
+
 int main(int argc, char *argv[]) {
-    int n = atoi(argv[1]);
+    int64_t n = strtoll(argv[1], NULL, 10);
     int degree = atoi(argv[2]);
+    std::cout << "n: " << n << std::endl;
 
     float *h_arr = new float[n];
     float *h_result = new float[n];
     float *h_coeffs = new float[n+1];
 
-    for(int i=0; i<n; i++){
+    for(int64_t i=0; i<n; i++){
         h_arr[i] = 1;
     }
     for (int i=0; i<=degree; i++){
@@ -54,12 +66,12 @@ int main(int argc, char *argv[]) {
     gpuErrchk(cudaStreamCreate(&stream));
 
     // Number of chunks
-    const int numChunks = n / chunk_size;
+    const int64_t numChunks = n / chunk_size;
     const int threadsPerBlock = 512;
     int blocksPerGrid = (chunk_size + threadsPerBlock - 1) / threadsPerBlock;
-
-    int offset;
-    for (int i = 0; i < numChunks; ++i) {
+    int64_t offset;
+    auto start_compute = std::chrono::high_resolution_clock::now();
+    for (int64_t i = 0; i < numChunks; ++i) {
         offset = i * chunk_size;
         
         gpuErrchk(cudaMemcpyAsync(d_arr_chunk, &h_arr[offset], chunk_size * sizeof(float), cudaMemcpyHostToDevice, stream));
@@ -69,8 +81,9 @@ int main(int argc, char *argv[]) {
         
         gpuErrchk(cudaStreamSynchronize(stream));
     }
+    auto end_compute = std::chrono::high_resolution_clock::now();
 
-    for(int i=0; i<n; i++){
+    for(int64_t i=0; i<n; i++){
         if (h_result[i] != degree+1){
             std::cout << "Result: " << h_result[i] << " At index: " << i <<  " is wrong" << std::endl;
             // Free allocated memory
@@ -84,6 +97,8 @@ int main(int argc, char *argv[]) {
             exit(0);
         }
     }
+
+    get_time_elapsed(start_compute, end_compute, n, degree);
 
     // Free allocated memory
     gpuErrchk(cudaFree(d_arr_chunk));
